6ac7659e25da1b9869422a8fbb7e50cbccb444cd
Anjiang <13271929138@163.com>
2017-05-15 17:21:47 +0800

    添加项目文件。

---
+++ AbstractFactory/Product.h
@@ -0,0 +1,48 @@
+#pragma once
+//Product.h
+
+#ifndef _PRODUCT_H_
+#define _PRODUCT_H_
+class AbstractProductA {
+public:
+	virtual ~AbstractProductA();
+	protected:  AbstractProductA();
+private:
+};
+class ProductA1 :public AbstractProductA {
+public:  ProductA1();
+		 ~ProductA1();
+protected:
+private:
+
+};
+class ProductA2 :public AbstractProductA {
+public:  ProductA2();
+		 ~ProductA2();
+protected:
+private:
+};
+
+
+class AbstractProductB {
+public:  
+	virtual ~AbstractProductB();
+	protected:  AbstractProductB();
+private:
+};
+
+class ProductB1 :public AbstractProductB {
+public:  ProductB1();
+		 ~ProductB1();
+protected:
+private:
+};
+class ProductB2 :public AbstractProductB {
+public:  ProductB2();
+		 ~ProductB2();
+protected:
+private:
+};
+
+
+#endif
